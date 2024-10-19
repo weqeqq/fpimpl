67b8623908c51c6ba564589a3e4d01e8e9a0835b
weqeqq <ggmodwear@gmail.com>
2024-10-19 18:32:36 +0300

    Update version

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         packages = rec {
           fpimpl = stdenv.mkDerivation {
             pname = "FPImpl";
-            version = "0.1.0";
+            version = "0.1.1";
             src = ./.;
 
             nativeBuildInputs = [ cmake ];
